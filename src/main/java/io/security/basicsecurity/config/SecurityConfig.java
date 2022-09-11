e6d1c2cb8c5bb26143b28ed23225b23b2bed0b00
이승민 <sml7179@naver.com>
2022-09-11 13:16:40 +0900

    사용자 정의 보안 기능 구현 SecurityConfig

---
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 이걸 넣어야 웹 보완이 활성화 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
+    }
+}
