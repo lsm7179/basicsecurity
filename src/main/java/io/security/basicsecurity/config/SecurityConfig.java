9b9854d1d5505133bfb69f39ed364f8936a55946
이승민 <sml7179@naver.com>
2022-09-11 14:39:52 +0900

    Form Login

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity // 이걸 넣어야 웹 보완이 활성화 된다.
@@ -12,9 +21,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated(); // 어떤 요청에도 인증이 안되면 접근이 안 된다라고 설정함.
 
         http
-                .formLogin();
+                .formLogin()
+                //.loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    System.out.printf("authentication"+ authentication.getName());
+                    httpServletResponse.sendRedirect("/");
+                })
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    System.out.println("exception"+ e.getMessage());
+                    httpServletResponse.sendRedirect("/login");
+                })
+                .permitAll();
     }
 }
