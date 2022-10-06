44f0447e1f8e8d9f580ce6330c69fa3546185775
이승민 <sml7179@naver.com>
2022-10-07 00:14:59 +0900

    authentication 확인

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,14 +1,34 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext securityContext = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = securityContext.getAuthentication();
+        // authentication authentication1 는 동일한 세션이다.
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread(){
+        new Thread(
+                () -> {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                }
+        ).start();
+        return "thread";
     }
 
     @GetMapping("loginPage")
